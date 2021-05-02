0af34c9fbbaeed94c7cb287841c61738b191accb
Hossain Rabbi <hossainrabbi2k1@gmail.com>
2021-05-02 16:48:49 +0600

    all finish

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import HashLoader from 'react-spinners/HashLoader';
 import NavBar from './components/NavBar/NavBar';
 import About from './components/About/About';
 import Blogs from './components/Blogs/Blogs';
@@ -9,6 +10,25 @@ import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 
 function App() {
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+        const timing = setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+
+        return () => clearTimeout(timing);
+    }, []);
+
+    if (loading) {
+        return (
+            <div className="loader">
+                <HashLoader color="#037fff" loading={loading} size={50} />
+            </div>
+        );
+    }
+
     return (
         <>
             <NavBar />
